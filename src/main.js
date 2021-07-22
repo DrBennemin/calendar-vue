3c688262502b2156fec08a50ac03ebdfa9b802a2
DrBennemin <benjamin.krzis@gmail.com>
2021-07-22 17:50:20 +0200

    Use loops for weekdays and dates and actual month and year by dayjs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./index.css";
+var dayjs = require("dayjs");
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format();
 
 createApp(App).mount("#app");
--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="container mx-auto">
-        <div class="my-12 font-bold">
-            Juli 2021
-        </div>
+        <div class="my-12 font-bold">{{ now }}</div>
         <div class="grid grid-rows-5 grid-cols-7 grid-flow-row gap-4">
-            <div class="weekday" v-for="(weekday, index) in weekdays" :key="index">
+            <div v-for="(weekday, index) in weekdays" :key="index">
                 {{ weekday }}
             </div>
-            <div class="border h-24 w-24 justify-self-auto" v-for="(date, index) in dates" :key="index">
+            <div
+                class="border h-24 w-24 grid justify-items-center items-center hover:bg-blue-500 hover:text-white hover:font-bold cursor-pointer"
+                v-for="(date, index) in dates"
+                :key="index"
+            >
                 {{ date }}
             </div>
         </div>
@@ -15,19 +17,22 @@
 </template>
 
 <script>
+import dayjs from "dayjs";
+
 export default {
     data() {
         return {
             weekdays: ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
             months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
             dates: null,
+            now: null,
         };
     },
     created() {
         for (let i = 0; i < 31; i++) {
             this.dates = i;
-            console.log(this.dates);
         }
+        this.now = dayjs().format("MMMM YYYY");
     },
 };
 </script>
