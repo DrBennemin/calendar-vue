40f57f2109eaa05e1c05b58d4b41f47b940a7ace
DrBennemin <benjamin.krzis@gmail.com>
2021-07-23 10:57:22 +0200

    Add clickfunctions

--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="container mx-auto">
-        <div class="my-12 font-bold">{{ now }}</div>
+        <div class="flex justify-between my-12">
+            <div class="font-bold">
+                {{ now }}
+            </div>
+            <div class="flex">
+                <button class="bg-blue-500 py-2 px-4 rounded-lg text-white hover:bg-red-500 mr-4" v-on:click="monthBack()">Back</button>
+                <button class="bg-blue-500 py-2 px-4 rounded-lg text-white hover:bg-red-500" v-on:click="monthNext()">Next</button>
+            </div>
+        </div>
         <div class="grid grid-rows-5 grid-cols-7 grid-flow-row gap-4">
             <div v-for="(weekday, index) in weekdays" :key="index">
                 {{ weekday }}
@@ -33,6 +41,18 @@ export default {
             this.dates = i;
         }
         this.now = dayjs().format("MMMM YYYY");
+    },
+    methods: {
+        monthBack: () => {
+            console.log(dayjs().subtract(1, "month"));
+        },
+        monthNext: () => {
+            let newMonth = dayjs().add(1, "month");
+            for (let i = 0; i < 1; i++) {
+                newMonth + 1;
+                console.log(newMonth);
+            }
+        },
     },
 };
 </script>
