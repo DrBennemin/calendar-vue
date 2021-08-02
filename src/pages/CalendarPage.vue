52318d8b26c7c4edbbaee0e16ffdc1463b8df724
DrBennemin <benjamin.krzis@gmail.com>
2021-08-02 14:37:18 +0200

    i am so stupid

--- src/pages/CalendarPage.vue
+++ src/pages/CalendarPage.vue
@@ -1,15 +1,11 @@
 <template>
     <div class="container mx-auto">
         <div class="flex justify-between my-12">
+            <button class="bg-blue-500 py-2 px-4 rounded-lg text-white hover:bg-red-500 mr-4" v-on:click="monthPrev">Prev</button>
             <div class="font-bold">
                 {{ now }}
             </div>
-            <div class="flex">
-                <button class="bg-blue-500 py-2 px-4 rounded-lg text-white hover:bg-red-500 mr-4" v-on:click="monthPrev += 1">Prev</button>
-                <button class="bg-blue-500 py-2 px-4 rounded-lg text-white hover:bg-red-500" v-on:click="monthNext += 1">Next</button>
-                <button class="bg-blue-500 py-2 px-4 rounded-lg text-white hover:bg-red-500" v-on:click="counter += 1">Counter</button>
-                <p>{{ counter }}</p>
-            </div>
+            <button class="bg-blue-500 py-2 px-4 rounded-lg text-white hover:bg-red-500" v-on:click="monthNext">Next</button>
         </div>
         <div class="grid grid-rows-5 grid-cols-7 grid-flow-row gap-6">
             <div v-for="(weekday, index) in weekdays" :key="index" class="grid justify-around">
@@ -38,7 +34,6 @@ export default {
             dates: null,
             now: null,
             counter: null,
-            // switchMonth: 1,
         };
     },
     created() {
@@ -47,51 +42,25 @@ export default {
             this.dates = i;
         }
     },
-    // updated: function() {
-    //     this.now = dayjs().format("MMMM YYYY");
-    // },
+
     methods: {
         monthPrev: function() {
-            this.counter = dayjs()
-                .subtract(this.counter, "month")
-                .format("MMMM YYYY");
-            // for (let i = 0; i < 100; i++) {
-            //     this.now = dayjs()
-            //         .subtract(i, "month")
-            //         .format("MMMM YYYY");
-            // }
-            // this.now = dayjs()
-            //     .subtract(1, "month")
-            //     .format("MMMM YYYY");
-            // let newValue = this.switchMonth++;
-            // newValue = this.switchMonth;
-            // this.now = dayjs()
-            //     .subtract(newValue, "month")
-            //     .format("MMMM YYYY");
-            // console.log(
-            //     dayjs()
-            //         .subtract(newValue, "month")
-            //         .format("MMMM YYYY")
-            // );
-        },
-        monthNext: function() {
-            this.counter = dayjs()
+            this.counter--;
+            console.log(this.counter);
+            this.now = dayjs()
                 .add(this.counter, "month")
                 .format("MMMM YYYY");
-            // let newValue = this.switchMonth++;
-            // newValue = this.switchMonth;
-            // this.now = dayjs()
-            //     .add(newValue, "month")
-            //     .format("MMMM YYYY");
-            // console.log(
-            //     dayjs()
-            //         .add(newValue, "month")
-            //         .format("MMMM YYYY")
-            // );
+        },
+        monthNext: function() {
+            this.counter++;
+            this.now = dayjs()
+                .add(this.counter, "month")
+                .format("MMMM YYYY");
         },
         pickDate: function(event) {
             alert(`Du hast auf den ${event.target.innerText}.ten geklickt`);
         },
+        showTodaysDate: function() {},
     },
 };
 </script>
